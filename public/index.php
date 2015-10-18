2b93784e8dcfbabd996eb67a13af205230008224
Katie <katiemcgoo@hotmail.co.uk>
2015-10-18 23:15:35 +0100

    css and images

--- public/index.php
+++ public/index.php
@@ -6,34 +6,37 @@
   </head>
 
   <body>
-      <div class="head box">
-        <a href="http://www.geniusenergy.co.uk"><img src="images/logo.png"/></a>
+      <div class="container">
+          
+          <div class="head ">
+            <a href="http://www.geniusenergy.co.uk"><img class="logo" src="images/logo-white-long.png"/></a>
+          </div>
+
+            <div class="form box">
+              <form action="mail.php" method="POST">
+                <input type="text" name="name" value="Name"><br>
+                <input type="tel" name="number" value="Phone"><br>
+                <input type="email" name="email" value="Email Address"><br>
+                <input type="text" name="postcode" value="Postcode"><br>
+                <select name="income" class="select">
+                  <option value="auto">Annual Income</option>
+                  <option value="1-2">£10,000 - £20,000</option>
+                  <option value="2-4">£20,000 - £40,000</option>
+                  <option value="4-8">£40,000 - £80,000</option>
+                  <option value="8">£80,000+</option>
+                </select><br>
+                <select name="home" class="select">
+                  <option value="auto">Do you own your home?</option>
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select><br>
+
+                <input class="btn" type="submit" value="Submit">
+
+              </form>
+                <p class="ftnote">A member of our team will contact you within 24 hours</p>
+          </div> 
       </div>
-      
-        <div class="form box">
-          <form action="mail.php" method="POST">
-            <input type="text" name="name" value="Name"><br>
-            <input type="tel" name="number" value="Phone"><br>
-            <input type="email" name="email" value="Email Address"><br>
-            <input type="text" name="postcode" value="Postcode"><br>
-            <select name="income">
-              <option value="auto">Annual Income</option>
-              <option value="1-2">£10,000 - £20,000</option>
-              <option value="2-4">£20,000 - £40,000</option>
-              <option value="4-8">£40,000 - £80,000</option>
-              <option value="8">£80,000+</option>
-            </select><br>
-            <select name="home">
-              <option value="auto">Do you own your home?</option>
-              <option value="yes">Yes</option>
-              <option value="no">No</option>
-            </select><br>
-            
-            <input type="submit" value="Submit">
-
-          </form>
-      </div> 
-
       
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         
